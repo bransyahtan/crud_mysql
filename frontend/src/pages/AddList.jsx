d3ce77e7c1ed29a38b1164539d5312e96f603865
bransyahtan <bransyahhhh@gmail.com>
2024-02-14 21:59:00 +0700

    complete crud

---
+++ frontend/src/pages/AddList.jsx
@@ -0,0 +1,107 @@
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate, Link } from "react-router-dom";
+
+export const AddList = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phone, setPhone] = useState("");
+
+  const navigate = useNavigate();
+
+  const saveUser = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:3000/users", {
+        name,
+        phone,
+        email,
+      });
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <div className="bg-slate-900 h-screen flex justify-center items-center">
+        <div className="w-1/3 h-auto bg-white rounded-2xl p-8 mt-8 mb-8">
+          <h1 className="text-3xl font-bold mb-4 text-center text-slate-900">
+            ADD LIST
+          </h1>
+          <form onSubmit={saveUser}>
+            <div className="mb-4">
+              <label
+                htmlFor="fullName"
+                className="block text-gray-700 text-sm font-bold mb-2"
+              >
+                Full Name
+              </label>
+              <input
+                type="text"
+                id="fullName"
+                name="fullName"
+                className="w-full px-3 py-2 border-2 border-black rounded-md"
+                placeholder="Your full name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                htmlFor="address"
+                className="block text-gray-700 text-sm font-bold mb-2"
+              >
+                Phone
+              </label>
+              <input
+                type="text"
+                id="address"
+                name="address"
+                className="w-full px-3 py-2 border-2 border-black rounded-md"
+                placeholder="Your Phone Number"
+                value={phone}
+                onChange={(e) => setPhone(e.target.value)}
+              />
+            </div>
+
+            <div className="mb-4">
+              <label
+                htmlFor="username"
+                className="block text-gray-700 text-sm font-bold mb-2"
+              >
+                Email
+              </label>
+              <input
+                type="text"
+                id="username"
+                name="username"
+                className="w-full px-3 py-2 border-2 border-black rounded-md"
+                placeholder="Your Email Address"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="w-full bg-slate-700 text-white font-bold py-2 px-4 rounded-md hover:bg-slate-900"
+            >
+              ADD LIST
+            </button>
+          </form>
+
+          <Link to="/">
+            <button
+              type="submit"
+              className="mt-3 w-full bg-slate-700 text-white font-bold py-2 px-4 rounded-md hover:bg-slate-900"
+            >
+              BACK
+            </button>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+};
